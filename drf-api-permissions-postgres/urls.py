5612a6fb30ca060955ad5175ca4341a50b739aba
issasalman <issasalmanhu1996@gmail.com>
2021-12-13 16:42:30 +0200

    done lab 32

--- drf-api-permissions-postgres/urls.py
+++ drf-api-permissions-postgres/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/ps5_games/', include('ps5_games.urls'))
+    path('api/v1/ps5_games/', include('ps5_games.urls')),
+    path("api-auth/", include("rest_framework.urls")),
+    
 ]
--- ps5_games/views.py
+++ ps5_games/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import Ps5_gamesSerialzer
 from .models import Ps5_games
+from .permissions import IsAuthenticatedOrReadOnly
 
 # CR views
 class Ps5_gamesList(generics.ListCreateAPIView):
     queryset = Ps5_games.objects.all()
     serializer_class = Ps5_gamesSerialzer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 # RUD view
 class Ps5_gamesDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ps5_games.objects.all()
     serializer_class = Ps5_gamesSerialzer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
